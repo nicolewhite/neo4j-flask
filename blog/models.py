70fd0501b33738724f5b80d7424d5c761bf48fdf
nicolewhite <nicole.white@neotechnology.com>
2015-01-09 17:43:16 -0800

    Update cypher query.

--- blog/models.py
+++ blog/models.py
@@ -67,10 +67,8 @@ class User:
         MATCH (u1:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag:Tag),
               (u2:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag)
         WHERE u1.username = {username} AND u1 <> u2
-        WITH u1, u2, COLLECT(DISTINCT tag.name) AS tags
-        WITH u2, tags, LENGTH(tags) AS len
-        ORDER BY len DESC
-        LIMIT 3
+        WITH u2, COLLECT(DISTINCT tag.name) AS tags, COUNT(DISTINCT tag) AS len
+        ORDER BY len DESC LIMIT 3
         RETURN u2.username AS similar_user, tags
         """
 
