79cf08b1ed75cedfdaaffcae9b55059999d23755
nicolewhite <nicole.white@neotechnology.com>
2015-01-16 15:55:43 -0800

    move some things around

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
 
 import models
 import views
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-from blog import *
+from blog import app
 import os
 
-port = int(os.environ.get("PORT", 5000))
+app.secret_key = os.urandom(24)
+port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)
