a81fa5f11eea7f33e8fccd6ea82ac0672963f8d0
nicolewhite <nicole.white@neotechnology.com>
2015-07-08 14:29:35 -0700

    Move the uniqueness constraints.
    Put the uniqueness constraints in __init__.py instead. Also use
    cypher instead of py2neo's graph.schema.

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,2 +1,11 @@
-import indexes
 from .views import app
+from .models import graph
+
+def create_uniqueness_constraint(label, property):
+    query = "CREATE CONSTRAINT ON (n:{label}) ASSERT n.{property} IS UNIQUE"
+    query = query.format(label=label, property=property)
+    graph.cypher.execute(query)
+
+create_uniqueness_constraint("User", "name")
+create_uniqueness_constraint("Tag", "name")
+create_uniqueness_constraint("Post", "id")
